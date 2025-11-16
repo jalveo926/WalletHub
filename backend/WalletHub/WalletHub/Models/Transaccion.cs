e1cb03bddd100dc7d208060c5c285ee8aef7571e
jalveo926 <jesusalveo445@gmail.com>
2025-11-16 16:07:07 -0500

    Se agregó el modelo transacción
    Cuando la rama modelos esté terminada se unirá a la rama backend

---
+++ backend/WalletHub/WalletHub/Models/Transaccion.cs
@@ -0,0 +1,10 @@
+﻿namespace WalletHub.Models
+{
+    public class Transaccion
+    {
+        public  int idTransaccion { get; set; } //El tipo int ya obliga a que tengan un valor
+        public string fechaTransac { get; set; }
+        public string montoTransac { get; set; }
+        public string descripcionTransac { get; set; }
+    }
+}
