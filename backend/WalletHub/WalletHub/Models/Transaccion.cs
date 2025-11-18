7c1e2e71f3166ddf1c86b831ebe498438f5eeab8
jalveo926 <jesusalveo445@gmail.com>
2025-11-17 20:27:37 -0500

    Squashed commit of the following:
    commit 00c8208ebca81ff4d52e986f217dfcc0940b3205
    Author: jalveo926 <jesusalveo445@gmail.com>
    Date:   Mon Nov 17 20:26:51 2025 -0500
    
        Squashed commit of the following:
    
        commit 3287c7d39357b9ee1162a433a92042ccbe8584b2
        Author: jalveo926 <jesusalveo445@gmail.com>
        Date:   Mon Nov 17 20:26:25 2025 -0500
    
            Remove ContextoDB and Servicios folders from project
    
            todavia no se usan así que es mejor poner estas carpetas cuando se usen
    
        commit 5f5886d51a1b655253c96de644d75877dda27251
        Author: YSIK <jessicazh0103@gmail.com>
        Date:   Mon Nov 17 20:06:54 2025 -0500
    
            Modelos
    
            Se crean los modelos para todas las entidades y las carpetas Servicio y ContextoDB

--- backend/WalletHub/WalletHub/Models/Transaccion.cs
+++ backend/WalletHub/WalletHub/Models/Transaccion.cs
@@ -3,8 +3,8 @@
     public class Transaccion
     {
         public  int idTransaccion { get; set; } //El tipo int ya obliga a que tengan un valor
-        public string fechaTransac { get; set; }
-        public string montoTransac { get; set; }
-        public string descripcionTransac { get; set; }
+        public DateTime fechaTransac { get; set; }
+        public decimal montoTransac { get; set; }
+        public string descripcionTransac { get; set; } 
     }
 }
