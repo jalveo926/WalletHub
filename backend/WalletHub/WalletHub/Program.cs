c2fe2ec6fda1413741a390ea8e66b78207e60266
Erick Hou <152131985+Perfekz@users.noreply.github.com>
2025-11-19 18:07:34 -0500

    FK configuration for DBContext

--- backend/WalletHub/WalletHub/Program.cs
+++ backend/WalletHub/WalletHub/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using WalletHub.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
