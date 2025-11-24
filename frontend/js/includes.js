958f9001dc954ca1c1fef23ddb13d1ae5eff1bd3
YSIK <jessicazh0103@gmail.com>
2025-11-24 09:19:55 -0500

    Add layout components and main frontend pages
    Introduces reusable header, nav, and footer components, a JS layout loader, and main HTML pages for dashboard, home, login, formulario, nosotros, reportes, and transaccion. Pages now use dynamic layout loading for consistent structure and easier maintenance.

---
+++ frontend/js/includes.js
@@ -0,0 +1,9 @@
+/*Función para darle layout a todas las pantallas que tienen los mismos elementos*/
+function cargarLayout(id, url) { /*Recibe como parámetros el id y la dirección del recurso*/ 
+    fetch(url) 
+        .then(response => response.text())
+        .then(html => {
+          document.getElementById(id).innerHTML = html;
+        })
+        .catch(err => console.error("Error cargando ${id}:", err)); 
+}
